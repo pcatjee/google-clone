4f53861690b92c1d3cab9fd6e95af08f25306678
Piyush <pcatjee@gmail.com>
2023-01-01 17:08:53 +0530

    img alt props fixed

--- pages/index.js
+++ pages/index.js
@@ -56,6 +56,7 @@ export default function Home() {
           src="https://i.imgur.com/FVp3jWG.png"
           width={300}
           height={100}
+          alt="Google Image"
         />
         <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
           <BsSearch className="h-5 mr-3 text-gray-500" />

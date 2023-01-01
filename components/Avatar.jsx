3784086eb54be02c1311f78cf69af871481d6307
Piyush <pcatjee@gmail.com>
2023-01-01 17:26:56 +0530

    image tag

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Avatar = ({ url, className }) => {
   return (
-    <Image
+    <img
       loading="lazy"
       className={`h-10 rounded-full cursor-pointer transition-duration-150 transform hover:scale-110 ${className}`}
       src={url}

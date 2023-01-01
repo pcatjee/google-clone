1811c85298ed77052f1edf5450d3374ebb2342cc
Piyush <pcatjee@gmail.com>
2023-01-01 15:19:13 +0530

    environ var applied

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 //API key -https://developers.google.com/custom-search/v1/using_rest
 //Always try to use environment variables.
+export const API_KEY = process.env.API_KEY;
 
-export const API_KEY = "";
-export const CONTEXT_KEY = "";
+export const CONTEXT_KEY = process.env.CONTEXT_KEY;
 
 //Context Key -https://cse.google.com/cse/create/new
